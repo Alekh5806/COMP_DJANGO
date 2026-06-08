f36f8772dcf2e759b1269cc0f1d549cf4b0a90b2
Alekh5806 <patelalekh5678@gmail.com>
2026-06-08 20:32:13 +0530

    the structure and the first app build is ready with the testing

---
+++ chaihq/tweet/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# here we are going to write the views for our tweet app, we will have a view for the home page, and a view for the tweet detail page. 
+
+def index(request):
+    return render(request, 'index.html')
+
